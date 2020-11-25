9de866106fea38cd9508089559562595a56065ae
KangMinseok <qbq5000@gmail.com>
2020-11-25 10:19:01 +0900

    [FEATURE] 프로필 구성

---
+++ WakeUp!/WakeUp!/Modules/Profile/ProfileViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileViewController.swift
+//  WakeUp!
+//
+//  Created by 강민석 on 2020/11/24.
+//
+
+import UIKit
+import Reusable
+import RxSwift
+import RxCocoa
+
+class ProfileViewController: BaseViewController, StoryboardSceneBased {
+    
+    static let sceneStoryboard = R.storyboard.profile()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func bindViewModel() {
+        super.bindViewModel()
+        
+        
+    }
+}
---
+++ WakeUp!/WakeUp!/Modules/Profile/ProfileViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewModel.swift
+//  WakeUp!
+//
+//  Created by 강민석 on 2020/11/24.
+//
+
+import RxSwift
+import RxCocoa
+
+class ProfileViewModel: BaseViewModel {
+ 
+    
+    
+    
+    
+    
+    
+    
+}
