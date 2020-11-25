ee6f1da05f32ddd2404305458e393fb60a7ee1b7
KangMinseok <qbq5000@gmail.com>
2020-11-25 10:02:12 +0900

    [FEATURE] Configs 파일 추가 앱 기본 설정 관리를 위하여 추가

---
+++ WakeUp!/WakeUp!/Configs/Configs.swift
@@ -0,0 +1,34 @@
+//
+//  Configs.swift
+//  WakeUp!
+//
+//  Created by 강민석 on 2020/11/24.
+//
+
+import UIKit
+
+struct Configs {
+
+    struct App {
+        static let bundleIdentifier = "com.tistory.axe-num1.WakeUp-"
+    }
+
+    // 외부 IP:
+    // 내부 IP:
+    struct Network {
+        static let baseURL = URL(string: "http://172.30.1.45:5000")!
+    }
+
+    struct BaseDimensions {
+
+    }
+
+    struct BaseColor {
+        
+    }
+
+    struct Path {
+
+    }
+
+}
